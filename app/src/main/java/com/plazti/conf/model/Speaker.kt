9b441777952a9fb4ea70535f21161ccfb532f742
juliansarmien18 <julian010422@gmail.com>
2021-07-18 21:56:21 -0500

    conexión a firebase y creación objetos Conference, Speaker y Ubication

---
+++ app/src/main/java/com/plazti/conf/model/Speaker.kt
@@ -0,0 +1,11 @@
+package com.plazti.conf.model
+
+class Speaker {
+    var name =""
+    var Jobtitle = ""
+    var workplace = ""
+    var biography = ""
+    var twitter = ""
+    var image = ""
+
+}
---
+++ app/src/main/java/com/plazti/conf/model/Ubication.kt
@@ -0,0 +1,11 @@
+package com.plazti.conf.model
+
+class Ubication {
+    val name = ""
+    val address = ""
+    val latitude = ""
+    val longitude = ""
+    val phone = ""
+    val website = ""
+    val photo = ""
+}
