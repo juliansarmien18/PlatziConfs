717462eb1cf7cec4264503ca25a99aaec32b0754
juliansarmien18 <julian010422@gmail.com>
2021-07-19 21:27:35 -0500

    listo para probar cronograma y expositores

--- app/src/main/java/com/plazti/conf/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/plazti/conf/view/ui/activities/MainActivity.kt
@@ -2,13 +2,19 @@ package com.plazti.conf.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.plazti.conf.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_main)
+        setActionBar(findViewById(R.id.toolbarmain))
+        configNav()
+    }
+    fun configNav(){
+        NavigationUI.setupWithNavController(bnv_menu, Navigation.findNavController(this, R.id.fragContent))
     }
 }
